844bb1bc38b50fadb3928c4015bea41645a322a2
Helene Andre <helene.andre.06@gmail.com>
2020-07-06 15:19:26 +1000

    Publish to gitPages.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-v4' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
 }
